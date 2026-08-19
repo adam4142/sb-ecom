cdceb865fa8eee006976b333f697714d015e54a1
Adam Philip <adamphilip9@gmail.com>
2026-08-19 19:10:12 +0530

    H2 DB & Repository added

--- src/main/java/com/ecommerce/sbecom/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/sbecom/service/CategoryServiceImpl.java
@@ -1,6 +1,8 @@
 package com.ecommerce.sbecom.service;
 
 import com.ecommerce.sbecom.model.Category;
+import com.ecommerce.sbecom.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
@@ -12,43 +14,50 @@ import java.util.Optional;
 
 @Service
 public class CategoryServiceImpl implements CategoryService{
-    private List<Category> categories = new ArrayList<>();
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    //private List<Category> categories = new ArrayList<>();
     private Long nextId = 1L;
 
     @Override
     public List<Category> getAllCategories() {
-        return categories;
+        return categoryRepository.findAll();
     }
 
     @Override
     public void createCategory(Category category) {
         category.setCategoryId(nextId++);
-        categories.add(category);
+        categoryRepository.save(category);
 
     }
 
     @Override
     public String deleteCategory(Long categoryId) {
+        List<Category> categories = categoryRepository.findAll();
         Category category = categories.stream()
                 .filter(c ->c.getCategoryId().equals(categoryId))
                 .findFirst().orElse(null);
         if(category == null){
             return "category not found";
         }
-        categories.remove(category);
+        categoryRepository.delete(category);
         return "Category with ID : "+categoryId+ "deleted";
     }
 
 
     @Override
     public Category updateCategory(Category category, Long categoryId) {
+        List<Category> categories = categoryRepository.findAll();
         Optional<Category> optionalCategory = categories.stream()
                 .filter(c ->c.getCategoryId().equals(categoryId))
                 .findFirst();
         if (optionalCategory.isPresent()){
             Category existingCategory = optionalCategory.get();
             existingCategory.setCategoryName(category.getCategoryName());
-            return  existingCategory;
+            Category savedCategory = categoryRepository.save(existingCategory);
+            return  savedCategory;
         }
         else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found");
